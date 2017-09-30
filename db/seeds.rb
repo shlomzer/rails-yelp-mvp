072de3c9be04f70008de128c2c9b70832f32a890
adrien salome <adrien.salome@hotmail.fr>
2017-09-30 15:09:52 +0200

    models controllers routes done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create!(name: 'Le Cafe des Stagiaires', address: 'Julu Lu', category: 'french')
+Restaurant.create!(name: 'Goodfellas', address: 'the Bund', category: 'italian')
+Restaurant.create!(name: 'Lost Heaven', address: 'Yan An Lu', category: 'chinese')
+Restaurant.create!(name: 'Cuivre', address: "Huaihai Zhong Lu", category: 'french')
+Restaurant.create!(name: "Wei Xiang Zhai", address: "Yandan Lu" , category: 'chinese')
